ed312f9dbd8ad2a5464923c3e50f3d2a080eff06
Bathula Ganesh <159684527+Ganesh-a0576@users.noreply.github.com>
2026-07-21 10:57:21 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        
+       int n=s1.length(),m=s2.length();
+       if(n>m) return false;
+       vector<int> a(26,0),b(26,0);
+       for(int i=0;i<n;i++){
+            a[s1[i]-'a']++;
+            b[s2[i]-'a']++;
+       } 
+       for(int i=0;i<m-n;i++){
+            if(a==b) return true;
+            b[s2[i]-'a']--;
+            b[s2[i+n]-'a']++;
+       }
+       return a==b;
+    }
+};
