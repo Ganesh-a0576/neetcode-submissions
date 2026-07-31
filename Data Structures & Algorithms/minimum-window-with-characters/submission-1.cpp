ad4d34be368cd8140998dbe7c0b2a8cd0fcac867
Bathula Ganesh <159684527+Ganesh-a0576@users.noreply.github.com>
2026-07-31 23:37:34 +0530

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        //edge cases
+        if(t.length() > s.length()) return "";
+        if(s==t) return s;
+        vector<int> nd(128,0);
+        int l=0,cnt=t.size(),mn=INT_MAX,st=0;
+        string ans;
+        for(char c:t) nd[c]++;
+        for(int r=0;r<s.size();r++){
+            if(nd[s[r]] > 0) cnt--;
+            nd[s[r]]--;
+
+            while(cnt==0){
+                if(r-l+1 < mn){
+                    mn=r-l+1;
+                    st=l;
+                }
+                nd[s[l]]++;
+                if(nd[s[l]] >0) cnt++;
+                l++;
+            }
+        }
+        return mn==INT_MAX?"":s.substr(st,mn);
+    }
+};
