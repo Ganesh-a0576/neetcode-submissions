53adab1d66633a24786b81fc96908bfba06c8106
Bathula Ganesh <159684527+Ganesh-a0576@users.noreply.github.com>
2026-09-03 22:30:02 +0530

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        double ans=1.0;
+        for(int i=1;i<m;i++){
+            ans=ans*(n-1+i)/i;
+        }
+        return (int)ans;
+    }
+};
