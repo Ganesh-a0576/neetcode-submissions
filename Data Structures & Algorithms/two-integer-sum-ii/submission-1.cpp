a59f67a78c5b20322edc65211103bb078fdb9b4c
Bathula Ganesh <159684527+Ganesh-a0576@users.noreply.github.com>
2026-09-19 12:38:44 +0530

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.cpp
@@ -0,0 +1,15 @@
+#pragma optimize("OFast","unroll-loops");
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        int i=0,j=nums.size()-1;
+        while(i<j){
+            int sm=nums[i]+nums[j];
+            if(sm==target) return {i+1,j+1};
+            else if(sm<target) i++;
+            else j--;
+        }
+        return {-1,-1};
+    }
+};
